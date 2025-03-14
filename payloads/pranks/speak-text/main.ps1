e73eb4fa3e160a98ee9a6638ec404214b942676f
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2025-03-14 21:27:36 +0100

    Update main.ps1

--- payloads/pranks/speak-text/main.ps1
+++ payloads/pranks/speak-text/main.ps1
@@ -1,3 +1,3 @@
 Function Speak-Text($Text) { Add-Type -AssemblyName System.speech; $TTS = New-Object System.Speech.Synthesis.SpeechSynthesizer; $TTS.Speak($Text) }
-Speak-Text "Ja ge Ji Jo jhu. You have been Powned"
+Speak-Text "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
 Exit
